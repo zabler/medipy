dd485074c20e8e802a7bdc2c52ef8df375653959
Nicolas Zabler <nicolas.zabler@gmx.de>
2021-01-14 10:43:52 +0100

    running

--- src/medipy/interfaces/ecg.py
+++ src/medipy/interfaces/ecg.py
@@ -15,31 +15,19 @@ class Ecg(metaclass=abc.ABCMeta):
     '''
     def __init__(self):
         self.samples = []
+        self.preprocessed = []
         self.sample_rate = None
-        self.delta_ms = None
+        self.period_ms = None
+        self.grid = None
         self.r_peaks = []
         self.rr_intervals = []
-        self.rr_missing = []
-    
-    @abc.abstractmethod
-    def data_reader(self, file):
-        '''
-        gets the data from files
-        '''
-        raise NotImplementedError
-    
-    @abc.abstractmethod
-    def save_to_table(self, file):
-        '''
-        saves the data in a table format
-        '''
-        raise NotImplementedError
+        self.rr_missings = []
 
     def save_object(self, file):
         '''
         saves the ecg object as pickle
         '''
-        with open(file, 'wb') as output_file: 
+        with open(file, 'wb') as output_file:
             pickle.dump(self, output_file, pickle.HIGHEST_PROTOCOL)
 
     def load_object(file):
@@ -48,14 +36,13 @@ class Ecg(metaclass=abc.ABCMeta):
         '''
         with open(file, 'rb') as input_file:
             return pickle.load(input_file)
-    
-    
-    def preprocessing_hamilton(self):
+
+    def preprocessor_hamilton(self):
         '''
-        preprocessing for hamilton algorithm
+        preprocesses a ecg signal for hamilton algorithm
         '''
         # Detrending
-        self.samples = signal.detrend(self.samples, axis= - 1, type='linear', bp=0, overwrite_data=False)
+        self.samples = signal.detrend(self.samples, axis=-1, type='linear', bp=0, overwrite_data=False)
 
         # Bandpass Filter
         f1 = 8/self.sample_rate
@@ -75,10 +62,13 @@ class Ecg(metaclass=abc.ABCMeta):
         a = [1]
         samples_ma = signal.lfilter(b, a, samples_rect)
         samples_ma[0:len(b) * 2] = 0  # Einschwingtiefe
-        self.preprocessed_hamilton = samples_ma
-    
-    def r_peak_detection_hamilton(self, least_distance=0.2, th_coefficient=0.45):
-        
+        self.preprocessed = samples_ma
+
+    def r_peak_detector_hamilton(self, least_distance=0.2, th_coefficient=0.45):
+        '''
+        This method detects all qrs patterns of ecg signal by hamilton algorithm
+        '''
+     
         # Initiation
         noisy_peaks = []
         noisy_peaks_average = 0.0
@@ -88,121 +78,133 @@ class Ecg(metaclass=abc.ABCMeta):
         rr_intervals = []
         rr_intervals_average = 0.0
         threshold = 0.0
-        counter=0
+        counter = 0
         index = []
-        peaks = []  
+        peaks = []
 
         # Hamilton Algorithmus
-        for counter in range(len(self.preprocessed_hamilton)):
+        for counter in range(len(self.preprocessed)): # evtl problematisch da kein 250 Hz grid fenster ENUMERATE
 
             # Peakdetektion
-            if counter>0 and counter<len(self.preprocessed_hamilton)-1: 
-                if self.preprocessed_hamilton[counter-1]<self.preprocessed_hamilton[counter] and self.preprocessed_hamilton[counter+1]<self.preprocessed_hamilton[counter]:
+            if counter > 0 and counter < len(self.preprocessed) - 1:
+                if self.preprocessed[counter - 1] < self.preprocessed[counter] and self.preprocessed[counter + 1] < self.preprocessed[counter]:
                     peak = counter
                     peaks.append(counter)
 
                     # R4 & R1
-                    if self.preprocessed_hamilton[counter] > threshold and (peak-qrs_peaks[-1])>least_distance*self.sample_rate: # MODIFIED to least_distance = 0.195
+                    if self.preprocessed[counter] > threshold and (peak - qrs_peaks[-1]) > least_distance * self.sample_rate: # MODIFIED to least_distance = 0.195
                         qrs_peaks.append(peak)
                         index.append(counter)
-                        safe_peaks.append(self.preprocessed_hamilton[peak])
-                        if len(safe_peaks)>8: #MODIFIED n to s
+                        safe_peaks.append(self.preprocessed[peak])
+                        if len(safe_peaks) > 8:  #MODIFIED n to s
                             safe_peaks.pop(0)
                         safe_peaks_average = np.mean(safe_peaks)
 
-                        # R5 
-                        if rr_intervals_average != 0.0: 
-                            if qrs_peaks[-1]-qrs_peaks[-2] > 1.5*rr_intervals_average: #Letztes RR Intervall größer als 1.5 mal RR Interval Average
-                                missed_peaks = peaks[index[-2]+1:index[-1]] # Suche nach Peaks zwischen dem jetzigen und dem letzten 
-                                for missed_peak in missed_peaks: # Gab es da einen Peak der 360ms vom letzten entfernt war? Thresshold nur die Hälft?
-                                    if missed_peak-peaks[index[-2]]>int(0.360*self.sample_rate) and self.preprocessed_hamilton[missed_peak]>0.5*threshold:
+                        # R5
+                        if rr_intervals_average != 0.0:
+                            if qrs_peaks[-1] - qrs_peaks[-2] > 1.5 * rr_intervals_average:  #Letztes RR Intervall größer als 1.5 mal RR Interval Average
+                                missed_peaks = peaks[index[-2] + 1:index[-1]]  # Suche nach Peaks zwischen dem jetzigen und dem letzten
+                                for missed_peak in missed_peaks:  # Gab es da einen Peak der 360ms vom letzten entfernt war? Thresshold nur die Hälft?
+                                    if missed_peak - peaks[index[-2]] > int(0.360 * self.sample_rate) and self.preprocessed[missed_peak] > 0.5 * threshold:
                                         qrs_peaks.insert(-2, missed_peak)  # Füge ihn hinzu TRUE PEAK AVERAGE? # MODIFIED INSTEAD SORT()
-                                        safe_peaks.insert(-2, missed_peak) # MODIFIED: Average neu 'CHECK THRESSHOLD
-                                        if len(safe_peaks)>8:
+                                        safe_peaks.insert(-2, missed_peak)  # MODIFIED: Average neu 'CHECK THRESSHOLD
+                                        if len(safe_peaks) > 8:
                                             safe_peaks.pop(0)
                                         safe_peaks_average = np.mean(safe_peaks) # True Peak Average
                                         break
 
-                        # R5                 
-                        if len(qrs_peaks)>2:
-                            rr_intervals.append(qrs_peaks[-1]-qrs_peaks[-2])
-                            if len(rr_intervals)>8:
+                        # R5             
+                        if len(qrs_peaks) > 2:
+                            rr_intervals.append(qrs_peaks[-1] - qrs_peaks[-2])
+                            if len(rr_intervals) > 8:
                                 rr_intervals.pop(0)
-                            rr_intervals_average = int(np.mean(rr_intervals)) # RR Interval Average
+                            rr_intervals_average = int(np.mean(rr_intervals))  # RR Interval Average
 
-                    # R4 
-                    else: 
-                        noisy_peaks.append(self.preprocessed_hamilton[peak])
-                        if len(noisy_peaks)>8:
+                    # R4
+                    else:
+                        noisy_peaks.append(self.preprocessed[peak])
+                        if len(noisy_peaks) > 8:
                             noisy_peaks.pop(0)
                         noisy_peaks_average = np.mean(noisy_peaks) # Noisy Peak Average
 
-                    threshold = noisy_peaks_average + th_coefficient*(safe_peaks_average-noisy_peaks_average) #Threshold Berechnung: Noisy Peaks Average + 0.45 Parameter * (True Peaks Average - Noisy Peaks Average)
-
-                    counter+=1
+                    threshold = noisy_peaks_average + th_coefficient*(safe_peaks_average-noisy_peaks_average) #Noisy Peaks Average + 0.45 Parameter * (True Peaks Average - Noisy Peaks Average)
+                    counter += 1
         
         #  Initial Wert + ersten zwei Werte entfernen
-        qrs_peaks.pop(0)  
         qrs_peaks.pop(0)
         qrs_peaks.pop(0)
-        self.r_peaks = qrs_peaks
-        # plt.figure()
-        # plt.plot(ecg.samples)
-        # plt.plot(QRS, ecg.samples[QRS],'x')
+        qrs_peaks.pop(0)
 
-    
+        #  R Peaks an Grid anpassen
+        self.r_peaks = [qrs_peak * self.period_ms for qrs_peak in qrs_peaks]
+
     def rr_interval_calculator(self):
         '''
         This method calculates all rr intervals of a given r_peak list
         '''
         for k in range(1, len(self.r_peaks)):
-            self.rr_intervals.append(math.ceil((self.r_peaks[k] - self.r_peaks[k - 1]) * self.delta_ms))
+            self.rr_intervals.append(math.ceil(self.r_peaks[k] - self.r_peaks[k - 1]))
     
+    def rr_interval_wrong_data_detector(self):
+        '''
+        This method detects all wrong rr_intervals
+        '''
+
+    def rr_interval_wrong_data_remover(self):
+        '''
+        This method removes all the wrong r peaks of the wrong rr_intervals
+        '''
+
     def rr_interval_missing_data_detector(self):
         '''
-        This method detects all rr abornamlties of a given rr list 
-        and returns a list about the values which have to be interpolated 
+        This method detects all missing rr_intervals
         '''
-        for index, rr in enumerate(self.rr):
+
+        for index, rr_interval in enumerate(self.rr_intervals):
+            if index <= 8:
+                self.rr_missings.append(0)
+                continue
+            rr_threshold = np.mean(self.rr_intervals[index - 9:index]) + np.std(self.rr_intervals[index - 9:index])
+            if rr_interval >= rr_threshold:
+                rr_missing = int(round(rr_interval / rr_threshold)) - 1
+                self.rr_missings.append(rr_missing)
+            else:
+                self.rr_missings.append(0)
+    
             # rr bigger als umgebung
             # rr größer als mean der letzten und kommenden 8 werte
             # dann sag artefact:
             # wert davor, wert danach, daraus anzahl der benötigten R zacken berechnen
         #return liste genauso lang wie rr_interval, aber mit int angabe der Werte die vom letzen R wert bis jetzt zu interpolieren sind 
-        
-        pass
     
-    def missing_data_check(self, rr_list, rr_missing, güte=0.1):
+    def rr_interval_interpolator(self, rr_list, method=None):  #time, rr_missing,
         '''
-        This method checks if enough actual values are here
+        This method interpolates rr_intevals based on time information
         '''
-        if sum(rr_missing)/len(rr_list) <= güte:
-            return True
-        else:
-            return False
-
-        #return summe für rr bereich über wie vieele R-zacken interpoliert werden müssen
-
-    def rr_interpolation(self, time, rr_list, rr_missing, method=None)
-        '''
-        Interpoliert rr_interval bei geggebenen rr_missung nach basierender methode
-        '''
-        if method == None:
+        if method is None:
             return rr_list
-        elif method = linear:
-                #scipy linare interpilation
+        elif method == 'linear':
+                #rr_interpolated = signal.interpolate.interp1d(,,kind=method) #kompliziert
+            rr_interpolated = rr_list[~np.isnan(rr_list)]
             return rr_interpolated
-    
-    def hrv_feature_sdann(self, rr_good)
+
+    def hrv_feature_sdnn(self, rr_intervals):
         '''
-        Berechnet das entsprechende feature, nimmt eine perfekte RR_liste an, bekomme keine weiteren Informatuionen
+        This method calculates sdnn based on a ideal rr_interval list
         '''
+
+    def hrv_feature_sdann(self, rr_intervals):
+        '''
+        This method calculates sdann based on a ideal rr_interval list
+        '''
+        feature = np.mean(rr_intervals) 
+        return feature
         #returnd value
         # to be interpoalted = missing_data_check(rr[])
         # kann evtl auch außerhalb der Funktion im calculator gefragt werdeb
         # elif summe der rr_missing aus diesem bereich == 10% aller rr_werte aus diesem Bereich
         # return np.nan als wert
         # elif
-        #   interpolate nach methode 
-        #   reutrn dann feature 
+        # interpolate nach methode 
+        # reutrn dann feature 
         # was wenn erster RR Wert rückwirkend interpolieren muss?, dann nein
--- src/medipy/realizations/ecg_freiburg.py
+++ src/medipy/realizations/ecg_freiburg.py
@@ -18,7 +18,152 @@ class EcgFreiburg(Ecg):
         self.tags = []
         self.seizures = []
         self.meta = None
+        self.feature_df = None
     
+    def data_reader(self, path):
+        '''
+        This method reads a ecg signal from edfplus in the format of uniklinik freiburg
+        '''
+        # Specific Input
+        read_list = ['EKG 1', 'EKG 2']
+
+        # Read In
+        signals, signal_headers, header = highlevel.read_edf(path, ch_names=read_list)
+        annotations = header['annotations']  #format Seconds.Milliseconds / Duration in sec / Tag: SEIZURE_P##_S##
+
+        # Signal
+        self.samples = signals[1] - signals[0]
+
+        # Sample Rate
+        self.sample_rate = signal_headers[0]['sample_rate']
+
+        # Period_ms
+        self.period_ms = int((1 / self.sample_rate) * 1000)
+        
+        # Grid
+        self.grid = np.arange(0, len(self.samples) * self.period_ms, self.period_ms, dtype=int)
+
+        # Tags
+        def annotation_to_grid(self, timestring):
+            sec, msec = str(timestring).split('.')
+            if len(msec) == 1:
+                msec = int(msec) * 100
+            elif len(msec) == 2:
+                msec = int(msec) * 10
+            else:
+                msec = int(msec)                
+            return int(((int(sec) * 1000) + msec) / self.period_ms)
+        
+        tags = ['SEIZURE']
+        for annotation in annotations:
+            if any(tag in annotation[2] for tag in tags):
+                self.seizures.append([annotation_to_grid(self, annotation[0]), annotation[2]])
+            else:
+                self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
+
+        # tags = ['SEIZURE']
+        # for annotation in annotations:
+        #     self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
+        #     if any(tag in annotation[2] for tag in tags):
+        #         self.seizures.append([annotation_to_grid(self, annotation[0]), annotation[2]])
+
+        # tags = ['SEIZURE']
+        # for annotation in annotations:
+        #     if any(tag in annotation[2] for tag in tags):
+        #         self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
+    
+    def short_time_hrv_extractor(self, window=300, overlap=0, include_meta=None, export=None,):
+        '''
+        st_hrv_calculator renntü ber eine PAndas table
+        Berechnet HRV Feature XY, basierend auf Zeitbasis als Index mit zentralem Speicherzeitpunkt, RRList im Zeitraum, und markierter Fehler in einem Zeitfenster von 5min überlappend ?
+        je nach FEature und je nach markierten Fehler 
+        in ecg freiburg
+        '''
+        # Feature Names
+        time_features = ['SDNN', 'SDRR', 'SDANN', 'SDNN Index', 'RMSSD', 'NN50', 'pNN50', 'DeltaHRMaxHRMin', 'TRI', 'TINN']
+        freq_features = ['VLF_Power', 'LF_Power', 'LF_rel', 'LF_Nu', 'LF_Peak', 'HF_Power', 'HF_rel', 'HF_Nu', 'HF_Peak', 'LF/HF']
+        non_linear_features = ['S', 'SD1', 'SD2', 'SD1/SD2', 'ApEn', 'SampEn', 'DF_alpha1', 'DF_alpha2', 'D_2']
+        
+        # Samples in Dataframe
+        samples_df = pd.DataFrame({'SAMPLES': self.samples}, index=self.grid)
+
+        # R-Peaks, RR_Intervals and RR_Missings to Dataframe
+        rr_intervals = self.rr_intervals
+        rr_intervals = np.insert(rr_intervals, 0, int(np.mean(self.rr_intervals[0:10])))
+        rr_missings = self.rr_missings
+        rr_missings = np.insert(rr_missings, 0, 0)
+        r_peaks_df = pd.DataFrame({'R_PEAKS': self.r_peaks,'RR_INTERVALS': rr_intervals, 'RR_MISSINGS': rr_missings}, index=self.r_peaks)
+        
+        # Combine in Signal Dataframe
+        signal_df = pd.concat([samples_df, r_peaks_df], ignore_index=False, axis=1).fillna(value=np.nan)
+        
+        # Get from Signal Dataframe including time information
+        rr_intervals_frame = signal_df['RR_INTERVALS'].to_numpy()
+        rr_missings_frame = signal_df['RR_MISSINGS'].to_numpy()
+        
+        # Create Feature Dataframe
+        self.feature_df = pd.DataFrame(np.nan, index=self.grid, columns=time_features+freq_features+non_linear_features)
+
+        # Calculation WRONG
+        half_window_ms = int(window / 2) * 1000
+        step = int(((1-overlap)*half_window_ms*2)/self.period_ms)
+        if step == 0:
+            step = 1
+        for time in self.grid[::step]: #Overlap damit nicht für jedes Sample ein Feature berechnet wird
+            if time < half_window_ms:
+                continue
+            rr_intervals_window = rr_intervals_frame[time - half_window_ms:time + half_window_ms]
+            rr_missings_window = rr_missings_frame[time - half_window_ms:time + half_window_ms]
+            if self.missing_data_check(rr_intervals_window[~np.isnan(rr_intervals_window)], rr_missings_window[~np.isnan(rr_missings_window)], güte=0.1):
+                self.feature_df.at[time, 'SDANN'] = self.hrv_feature_sdann(self.rr_interval_interpolator(rr_intervals_window, method='linear'))
+        
+        if include_meta is not None:
+
+            # Tags in Dataframe
+            tags_values = []
+            tags_index = []
+            for tag in self.tags:
+                tags_values.append(tag[1])
+                tags_index.append(tag[0])
+            tags_df = pd.DataFrame({'TAGS': tags_values}, index=tags_index)
+            # INDEX FÜR SYSTEMATISCHE OTHER SEIZURES FEHLT, MÜSSTE ANNOTIERT WERDEN
+            # SIND ABER IN TAGS ENTHALTEN 
+            
+            # Seizures in Dataframe
+            seizures_values = []
+            seizures_index = []
+            for seizure in self.seizures:
+                seizures_values.append(seizure[1])
+                seizures_index.append(seizure[0])
+            seizures_df = pd.DataFrame({'SEIZURES': seizures_values}, index=seizures_index)
+
+            # Metas of Seizures in Dataframe
+            meta_df = pd.read_csv(include_meta, delimiter=';')
+            meta_df = meta_df[meta_df['TAG'].isin(seizures_values)].reset_index(drop=True)
+            meta_df = meta_df.set_index(seizures_df.index)
+            self.meta = meta_df.values.tolist()
+
+             # Alle Spalten Droppen die für weitere Verarbeitung irrelavant sind
+            meta_df = meta_df.drop(columns=['MYOCLONIC','EPILEPTIC','TYP I','TYP II','TYP III','SEITE','TAG','STUDY_ID','ORIGIN_START','EMG','DELRE','DELLI','QUADRE','QUADLI','VIDEO','VIGILANZ','PERCEPTION','IMPULSE'])
+
+            # Merge Dataframe
+            self.feature_df = pd.concat([self.feature_df, seizures_df, meta_df], ignore_index=False, axis=1) #tags_df
+        
+        if export is not None:
+            #Save Dataframe to pickle (evtl. zu Testzwecken als csv)
+            self.feature_df.to_pickle(export)
+
+    def missing_data_check(self, rr_list, rr_missing, güte=0.1):
+        '''
+        This method checks if there are enough non interpolated values
+        '''
+        #check 5min mindestwert an rr listen, was wenn rr_missing werte erst im anschluss kommen , plausbiel check mean rr -> Anzahl der mind peaks
+        if len(rr_list) > 150: #30 BPM 5 min  
+            if sum(rr_missing)/len(rr_list) <= güte:
+                return True
+        else:
+            return False
+
     def plot_tag_in_ecg(self, tag, sec_pre=20, sec_post=40):
         '''
         plots a part of the signal in an extra plot
@@ -53,141 +198,3 @@ class EcgFreiburg(Ecg):
         new_time = np.linspace(-1 * sec_pre, sec_post, len(new_xticks), endpoint=True, dtype=str)
         plt.gca().set_xticklabels(new_time)
         plt.show()#block=False)
-
-
-    def data_reader(self, path):
-        '''
-        This method reads a ecg signal from edfplus in the format of uniklinik freiburg
-        '''
-        # Specific Input
-        read_list = ['EKG 1', 'EKG 2']
-
-        # Read In
-        signals, signal_headers, header = highlevel.read_edf(path, ch_names=read_list)
-        annotations = header['annotations']  #format Seconds.Milliseconds / Duration in sec / Tag: SEIZURE_P##_S##
-
-        # Signal
-        self.samples = signals[1] - signals[0]
-
-        # Sample Rate
-        self.sample_rate = signal_headers[0]['sample_rate']
-        self.delta_ms = int((1/self.sample_rate)*1000)
-
-        # Tags
-        def annotation_to_grid(self, timestring):
-            sec, msec = str(timestring).split('.')
-            if len(msec) == 1:
-                msec = int(msec) * 100
-            elif len(msec) == 2:
-                msec = int(msec) * 10
-            else:
-                msec = int(msec)                
-            return int(((int(sec) * 1000) + msec)/self.delta_ms)
-
-        # tags = ['SEIZURE']
-        # for annotation in annotations:
-        #     self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
-        #     if any(tag in annotation[2] for tag in tags):
-        #         self.seizures.append([annotation_to_grid(self, annotation[0]), annotation[2]])
-                
-        tags = ['SEIZURE']
-        for annotation in annotations:
-            if any(tag in annotation[2] for tag in tags):
-                self.seizures.append([annotation_to_grid(self, annotation[0]), annotation[2]])
-            else:
-                self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
-
-        # tags = ['SEIZURE']
-        # for annotation in annotations:
-        #     if any(tag in annotation[2] for tag in tags):
-        #         self.tags.append([annotation_to_grid(self, annotation[0]), annotation[2]])
-    
-    def short_time_hrv_calculator(self, window=2, overlap=1):
-        '''
-        st_hrv_calculator renntü ber eine PAndas table
-        Berechnet HRV Feature XY, basierend auf Zeitbasis als Index mit zentralem Speicherzeitpunkt, RRList im Zeitraum, und markierter Fehler in einem Zeitfenster von 5min überlappend ?
-        je nach FEature und je nach markierten Fehler 
-        in ecg freiburg
-        '''
-        # Feature Names
-        time_features = ['SDNN','SDRR','SDANN','SDNN Index','RMSSD','NN50','pNN50','DeltaHRMaxHRMin','TRI','TINN']
-        freq_features = ['VLF_Power','LF_Power','LF_rel','LF_Nu','LF_Peak','HF_Power','HF_rel','HF_Nu','HF_Peak','LF/HF']
-        non_linear_features = ['S', 'SD1', 'SD2', 'SD1/SD2', 'ApEn', 'SampEn', 'DF_alpha1', 'DF_alpha2', 'D_2']
-        
-        # Samples in Dataframe
-        samples_df = pd.DataFrame({'SAMPLES': self.samples})
-
-        # R-Peaks to Dataframe
-        rr_intervals = self.rr_intervals.insert(0, np.mean(self.rr_intervals[0:10]))
-        rr_missings = self.rr_missing.insert(0, 0)
-        r_peaks_df = pd.DataFrame({'R_PEAKS': rr_intervals, 'RR_MISSING': rr_missings}, index=self.r_peaks)
-
-        # Iterate Dataframe
-        work_df = pd.concat([samples_df, r_peaks_df], ignore_index=False, axis=1)
-
-        # Feature Dataframe
-        feature_df = pd.DataFrame(index=work_df.index,columns=time_features+freq_features+non_linear_features) für alle Features dataframe mit nans und index time
-
-        for work_index, row in work_df.iterrows(): # notwenig über diesen zu itterieren oder über range sinnvoller?# evtl nur über den index # pd besser da nicht kompletter satensatz evtl gelesen wird?
-            if work_index < self.samplerate * int(window/2):
-                break
-            current_rr_interval = work_df.iloc[work_index-150:work_index*150,1] #ignore np.nan # work index vermutlich andere werte # nur mit pandas einfach einsammeln der zracken im hießigen bereich möglich
-            current_rr_missing = work_df.iloc[work_index-150:work_index*150,2]
-            if self.missing_data_check(current_rr_interval, current_rr_missing, güte=0.1):
-                # alle funktionen nacheindnaer aufrufen, rr werte und misssing geben, interpolieren, feature berchnen und in feature tabelle einfügen mittelsindex
-                feature_df.set_value(workd_index, 'SDANN', ecg.hrv_feature_sdann(ecg.rr_interpolation(method=linear)))
-
-            # elif für andere güte durchführbar, bsp für 20% fehlende werte, andere interpolations methodik
-            else:
-                pass
-                # für alle nan schrieben, da nan schon gegebn gehe weiter
-            #work_index hochzählen anhand overlapping
-            #damit nicht für jeden Sample etwas gerechnet wird 
-                
-        
-        # über index lösen
-        # HRV Extraktion
-
-    def export_feature_table_with_classes(self, feature_table,file, meta_path):
-        '''
-        saves the data as a pandas table in pickle
-        '''
-
-        # Tags in Dataframe
-        tags_values = []
-        tags_index = []
-        for tag in self.tags:
-            tags_values.append(tag[1])
-            tags_index.append(tag[0])
-        tags_df = pd.DataFrame({'TAGS': tags_values}, index=tags_index)
-        
-        # Seizures in Dataframe
-        tags_values = []
-        tags_index = []
-        for tag in self.tags:
-            tags_values.append(tag[1])
-            tags_index.append(tag[0])
-        tags_df = pd.DataFrame({'TAGS': tags_values}, index=tags_index)
-
-        # Metas of Seizures in Dataframe
-        meta_df = pd.read_csv(meta_path, delimiter=';')
-        meta_df = meta_df[meta_df['TAG'].isin(tags_values)].reset_index(drop=True)
-        meta_df = meta_df.set_index(tags_df.index)
-        self.meta = meta_df.values.tolist()
-
-        # Alle Spalten Droppen die für weitere Verarbeitung irrelavant sind
-        meta_df = meta_df.drop(columns=['MYOCLONIC','EPILEPTIC','TYP I','TYP II','TYP III','SEITE','TAG','STUDY_ID','ORIGIN_START','EMG','DELRE','DELLI','QUADRE','QUADLI','VIDEO','VIGILANZ','PERCEPTION','IMPULSE'])  
-        
-        # # Samples in Dataframe
-        # samples_df = pd.DataFrame({'SAMPLES': self.samples})
-
-        # # R-Peaks in Dataframe
-        # rr_intervals = self.rr_intervals.insert(0, np.nan)
-        # r_peaks_df = pd.DataFrame({'R_PEAKS': rr_intervals}, index=self.r_peaks)
-
-        # Merge Dataframe
-        all_df = pd.concat([feature_table, tags_df, meta_df], ignore_index=False, axis=1)
-
-        #Save Dataframe to pickle (evtl. zu Testzwecken als csv)
-        all_df.to_pickle(file)
-
